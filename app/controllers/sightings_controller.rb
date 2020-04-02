8004ffb538497f027247719106f19a276bf762b1
Olusola Olagoroye <solaolagoroye@gmail.com>
2020-04-02 04:02:54 -0500

    Done.

---
+++ app/controllers/sightings_controller.rb
@@ -0,0 +1,8 @@
+class SightingsController < ApplicationController
+
+    sighting = Sighting.find_by(id: params[:id])
+  render json: sighting.to_json(:include => {
+    :bird => {:only => [:name, :species]},
+    :location => {:only => [:latitude, :longitude]}
+  }, :except => [:updated_at])
+end
